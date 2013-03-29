3a81fa2d104381e4156679e570a976ba2485a6d0
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2013-03-29 14:52:49 +0400

    Добавил разделение по категориям

---
+++ pavilion_list_parser.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open("http://www.respublika-nn.ru/?id=202"))
+
+#pavilions = doc.css(".list_table_cols a:not([href*='plan'])");
+categories = doc.css("ul.cell_standart_struct1");
+
+categories.each do |category|
+	title = category.css("li.cell_standart_struct1 .menuchilds").text
+	puts "#{title} ---->"
+
+	pavilions = category.css("a:not([href*='plan'])")
+
+	pavilions.each do |pavilion|
+		puts "#{pavilion.text} - #{pavilion['href']}"
+	end
+end
