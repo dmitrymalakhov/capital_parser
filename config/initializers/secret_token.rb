617c6bce95cef2bd2d62306656bd2e4d2f08b142
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2013-04-04 09:03:10 +0400

    Создан проект для управления парсерами

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapitalAdmin::Application.config.secret_token = '3066205f9f1e340e132f0870ba05926edcc464ec9b6316b3a2f4e36fadc855437da9cddb006c39c9fdd66d7ba6b46649dacd748d159103816325f6648d881f21'
