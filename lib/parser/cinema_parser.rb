736e7b894a6e1a8d36a26ef75ae0140b638a3812
dmitry <dmitry@MadGest.(none)>
2013-05-20 21:54:33 +0400

    Выгруженна текстовая информация по фильмам без описания

--- lib/parser/cinema_parser.rb
+++ lib/parser/cinema_parser.rb
@@ -18,10 +18,35 @@ class CinemaParser < ParserBase
 				film_path = schedule.css('.msch_group_title a').xpath('@href')
 				
 				film = get_document("#{@store.cinema_url}#{film_path}")
+				film.css("#film_title #film_title_formats").remove
 
-				film_title = film.css(".film_title").text
+				puts film_path
+				film_title = film.css("#film_title").text
+				film_engtitle = film.css("#film_engtitle").text
+				
+				genre, duration, in_theaters, production, producer, actors = "", "", "", "", "", ""
+				film.css("#film_bits div").each do |bit|
+					case bit.text
+						when /жанр/i
+							genre = bit.css("b").text
+						when /длительность/i
+							duration = bit.css("b").text
+						when /в прокате/i
+							in_theaters = bit.css("b").text
+						when /производство/i
+							production = bit.css("b").text
+					end
+				end
 
-				Film.where(:title => film_title).first_or_create(:title => film_title)		
+				film.css(".film_attrs").each do |attribute|
+					case attribute.css(".film_attr_header").text
+						when /режиссёр/i
+							producer = attribute.css(".film_attr_data").text
+						when /актёры/i
+							actors = attribute.css(".film_attr_data").text
+					end
+				end
+				Film.where(:title => film_title).first_or_create(:title => film_title, :engtitle => film_engtitle, :genre => genre, :duration => duration, :in_theaters => in_theaters, :production => production, :producer => producer, :actors => actors)		
 			end
 		end
 	end
