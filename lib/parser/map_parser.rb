59ed004d9841cc3d43d768a03b94feba58a1fb47
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2013-06-17 10:11:01 +0400

    Работа с полилиниями

--- lib/parser/map_parser.rb
+++ lib/parser/map_parser.rb
@@ -7,56 +7,56 @@ class MapParser < ParserBase
 	def update
 		doc = get_xml("maps/#{@store.id}/1.xml")
 
-		Dir.foreach("maps/#{@store.id}") {|file| 
+		Dir.foreach("maps/#{@store.id}") {|file|
 			if file =~ /.xml/
 
 				temp = /.*?([\d]+).*/.match  file
-				floor = temp[1] 
+				floor = temp[1]
 
 				doc.css("g").each do |group|
 					# group = doc.css("g").first
-					
+
 					items = group.xpath("*")
-					
-					primitives = items.map { |item| 
+
+					primitives = items.map { |item|
 						Primitive.new(item)
 					}
 
 					path_closed, index = 0, 0
 
-					puts primitives.count()
-					until path_closed == primitives.count()-1					
+					# puts primitives.count()
+					until path_closed == primitives.count()-1
 
-						puts "#p = #{path_closed} - i = #{index+1}"
-						puts "#{primitives[path_closed].x2} --- #{primitives[index+1].x1} --- #{primitives[path_closed].y2} --- #{primitives[index+1].y1}"
+						# puts "#p = #{path_closed} - i = #{index+1}"
+						# puts "#{primitives[path_closed].x2} --- #{primitives[index+1].x1} --- #{primitives[path_closed].y2} --- #{primitives[index+1].y1}"
 						if (primitives[path_closed].x2 == primitives[index+1].x1) and primitives[path_closed].y2 == primitives[index+1].y1
 							primitives[path_closed+1], primitives[index+1] = primitives[index+1], primitives[path_closed+1]
 							path_closed += 1
 							index = path_closed
-							puts "ok"
+							# puts "ok"
 						elsif primitives[path_closed].x2 == primitives[index+1].x2 and primitives[path_closed].y2 == primitives[index+1].y2
 							primitives[index+1].invert
 							primitives[path_closed+1], primitives[index+1] = primitives[index+1], primitives[path_closed+1]
 							path_closed += 1
 							index = path_closed
-							puts "revers ok"
+							# puts "revers ok"
 						else
 							index = index == primitives.count()-2 ? path_closed : index += 1
 						end
 
-						
+
 					end
 
-					d = ""
-					path = primitives.each_with_index { |item, index|
-						d += item.path(index)
-					}
-					
-					d += "z"
+					# d = ""
+					# path = primitives.each_with_index { |item, index|
+					# 	d += item.path(index)
+					# }
 
-					@store.regions.where(:path => d, :floor => floor).first_or_create
+					# d += "z"
+
+					# @store.regions.where(:path => d, :floor => floor).first_or_create
 				end
-			end 
+			end
 		}
 	end
 end
--- lib/parser/primitive.rb
+++ lib/parser/primitive.rb
@@ -10,20 +10,20 @@ class Primitive
 	def path(pos)
 		case @type
 			when "line"
-				if pos == 0 
+				if pos == 0
 					"M#{@x1},#{@y1}L#{@x2},#{@y2}"
 				else
 					"L#{@x2},#{@y2}"
 				end
-			when "path"
+			when "polyline"
 
 		end
 	end
-		
+
 	def invert
 		@x1, @y1, @x2, @y2 = @x2, @y2, @x1, @y1
 	end
-	
+
 	private
 	def generate
 		case @node.name
@@ -34,7 +34,20 @@ class Primitive
 				@x2 = @node.xpath('@x2').to_s
 				@y2 = @node.xpath('@y2').to_s
 
-			
+			when "polyline"
+
+				points = @node.xpath('@points').to_s
+
+				points = points.split(" ")
+
+				begin_point = points.first.split(",")
+				end_point = points.last.split(",")
+
+				@x1 = begin_point[0]
+				@y1 = begin_point[1]
+				@x2 = end_point[0]
+				@y2 = end_point[1]
+
 		end
-	end 
+	end
 end
