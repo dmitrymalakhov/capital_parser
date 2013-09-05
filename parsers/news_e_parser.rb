b21c90237d22475a131c1439e2b5cb323eb4a60d
Dmitry M <dmitrii.malakhov@gmail.com>
2013-09-05 14:09:21 +0400

    fix db

--- parsers/news_e_parser.rb
+++ parsers/news_e_parser.rb
@@ -7,27 +7,27 @@ doc.xpath('//@style').remove
 doc.search('script').remove
 doc.css('.view_icon_div').remove
 
-# news = doc.css(".block_is td.art_sect").first
-doc.css(".block_is td.art_sect").each do |news|
+news = doc.css(".block_is td.art_sect").first
+# doc.css(".block_is td.art_sect").each do |news|
 	date = news.css(".date").text	
 	title = news.css("a.menunews").text
 	path = news.css("a.menunews").xpath('@href').text
 
-	puts path
+	# puts path
 	article = Nokogiri::HTML(open("http://www.etagi.ru#{path}"))
 	article.xpath('//@style').remove
 	article.search('script').remove
 
 	content = article.css(".block_is table[cellpadding='5']")
-	puts content
+	# puts content
 	content.css("img").each do |img|
 		src = img.xpath('@src').text
-		puts "#{src}"
+		# puts "#{src}"
 	end
 	content.search('img, hr').remove
+	text = content.text
+	puts "#{text.squeeze('\t')}"
 
-	puts "#{content.text}"
-
-end
+# end
 
  
