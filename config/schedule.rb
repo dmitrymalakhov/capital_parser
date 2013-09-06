8ad46b5a138f469f4a33247b4f0c3791a4196b53
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2013-09-06 13:48:58 +0400

    fix cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+require 'open-uri'
+require 'parser/parser_base'
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
